dcd409b5973454f640d709cabb70785192493e84
micahcourey <micahjcourey@gmail.com>
2018-12-15 23:22:02 -0800

    add build folder

---
+++ build/precache-manifest.8ec1367ea69376be8f5a6937daf7755f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b284d8478319d1070b0e",
+    "url": "/static/js/main.b284d847.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "b284d8478319d1070b0e",
+    "url": "/static/css/main.4f388ae6.chunk.css"
+  },
+  {
+    "revision": "6bdde2163dd33f8ef8d62406c33cd458",
+    "url": "/index.html"
+  }
+];
